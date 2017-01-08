bd77d7e3135ea92d0d34ed6060c3d9f0e9b4c7db
Henry Goddard <godd9170@mylaurier.ca>
2017-01-07 19:00:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## vue-test
+
+Just messing around with `vue.js` to see what the hype is. Followed `https://skyronic.com/blog/vue-project-scratch` to get started.
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
+  }
+}
